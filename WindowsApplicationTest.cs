54dc93e13b07d0bc3c2f395ad65569845c663111
shuang-twist <91921175+shuang-twist@users.noreply.github.com>
2022-03-08 10:33:09 -0800

    Add project files.

---
+++ WindowsApplicationTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimpleTest
+{
+    public partial class WindowsApplicationTest : ServiceBase
+    {
+        public WindowsApplicationTest()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            File.AppendAllText(@"C:\temp\logForSimpleTest.txt", "service started");
+        }
+
+        protected override void OnStop()
+        {
+            File.AppendAllText(@"C:\temp\logForSimpleTest.txt", "service stopped");
+        }
+    }
+}
